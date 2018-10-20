a2e4ec8ae6dcb005d49b76abf1e8f911f846f113
Hakan Aksüt <hakan.aksut58@gmail.com>
2018-10-20 20:34:16 +0300

    temel arayuz

---
+++ src/main/java/com/yavuzunver/tulumbaci/Main4Activity.java
@@ -0,0 +1,33 @@
+
+package com.yavuzunver.tulumbaci;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
+import android.net.Uri;
+
+public class Main4Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main4);
+
+
+
+        ((Button) findViewById(R.id.button2)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri gmmIntentUri = Uri.parse("geo:37.7749,-122.4194");
+                Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+                mapIntent.setPackage("com.google.android.apps.maps");
+                if (mapIntent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(mapIntent);
+                }
+
+            }
+        });
+    }
+}
--- src/main/java/com/yavuzunver/tulumbaci/MainActivity.java
+++ src/main/java/com/yavuzunver/tulumbaci/MainActivity.java
@@ -1,17 +1,20 @@
 package com.yavuzunver.tulumbaci;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView. OnItemClickListener;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.content.Intent;
 import android.widget.ListView;
+
 
 public class MainActivity extends AppCompatActivity {
 
     private String[] ulkeler =
-            {"Liste1","Liste2"};
+            {"Liste1", "Liste2"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,20 +25,32 @@ public class MainActivity extends AppCompatActivity {
         ((Button) findViewById(R.id.button1)).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i= new Intent(MainActivity.this,Main2Activity.class);
+                Intent i = new Intent(MainActivity.this, Main2Activity.class);
                 startActivity(i);
             }
         });
 
 
-        ListView listemiz=(ListView)findViewById(R.id.listView1);
-        ArrayAdapter<String> veriAdaptoru=new ArrayAdapter<String>
+        ListView listemiz = (ListView) findViewById(R.id.listView1);
+        ArrayAdapter<String> veriAdaptoru = new ArrayAdapter<String>
                 (this, android.R.layout.simple_list_item_1, android.R.id.text1, ulkeler);
         listemiz.setAdapter(veriAdaptoru);
 
 
+        //Dosya Adı: MainActivity.java
+
+        listemiz.setOnItemClickListener(new OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent i = new Intent(MainActivity.this, Main4Activity.class);
+                startActivity(i);
+
+
+            }
+        });
+
 
     }
-
 
 }
